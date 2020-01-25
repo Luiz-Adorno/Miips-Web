635c7396d762cf58e927060c927ac79553e709b5
Luiz Adorno <luizadorno96@gmail.com>
2020-01-24 21:51:11 -0300

    iniciando mudanças no index.html

--- scripts/auth_index.js
+++ scripts/auth_index.js
@@ -1,18 +1,41 @@
 auth.onAuthStateChanged(function(user) {
     if (user) {
       // User is signed in.
-     // window.location.href = '/painel.html';
-      console.log('xxx');
+      console.log('logged');
       // ...
+      document.getElementById("singup-form").style.display = "none";
+      document.getElementById("cadastro-mensagem").style.display = "none";
+
     } else {
       // User is signed out.
-      console.log('zzz');
+      console.log('signed out');
       // ...
     }
+
+    document.getElementById("singup-form").style.display = "none";
+    document.getElementById("cadastro-mensagem").style.display = "none";
   });
 
-  /*  firebase.auth().signOut().then(function() {
+  firebase.auth().signOut().then(function() {
     // Sign-out successful.
   }, function(error) {
     // An error happened.
-  });*/
+  });
+
+  const singup = document.querySelector('#singup-form');
+
+  singup.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const email = singup['email'].value;
+    const password = singup['password'].value;
+
+    firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // ...
+    });
+
+  })
+
