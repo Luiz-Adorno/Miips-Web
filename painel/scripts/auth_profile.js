0fc5b9a4bc19f9bc407b59b64eb0d359ddc38011
Luiz Adorno <luizadorno96@gmail.com>
2020-02-04 18:49:32 -0300

    firestore responsible auth implemented

--- painel/scripts/auth_profile.js
+++ painel/scripts/auth_profile.js
@@ -1,26 +1,40 @@
 auth.onAuthStateChanged(function (user) {
-    if (user) {
-        // User is signed in.
-        var email = user.email;
-        var uid = user.uid;
-        console.log(email, uid);
+  if (user) {
+    // User is signed in.
+    var email = user.email;
+    var uid = user.uid;
+    console.log(email, uid);
 
-        document.getElementById('avatar_big').src=user.photoURL
-        document.getElementById('avatar_small').src=user.photoURL
-    } else {
-        // User is signed out.
-        console.log('signed out');
-        window.location.href = '/login.html';
+    var docRef = db.collection("companies").doc(uid);
 
-        // ...
-    }
+    docRef.get().then(function (doc) {
+      if (doc.exists) {
+        console.log("Document data:", doc.data());
+      } else {
+        // doc.data() will be undefined in this case
+        console.log("No such document!");
+      }
+    }).catch(function (error) {
+      console.log("Error getting document:", error);
+    });
+
+    document.getElementById('avatar_big').src = user.photoURL
+    document.getElementById('avatar_small').src = user.photoURL
+  } else {
+    // User is signed out.
+    console.log('signed out');
+    window.location.href = '/login.html';
+
+    // ...
+  }
 
 });
 
 document.getElementById("btn_logout").addEventListener('click', function () {
-    firebase.auth().signOut().then(function () {
-      // Sign-out successful.
-    }, function (error) {
-      // An error happened.
-    });
+  firebase.auth().signOut().then(function () {
+    // Sign-out successful.
+  }, function (error) {
+    // An error happened.
   });
+});
+
--- scripts/auth_person.js
+++ scripts/auth_person.js
@@ -2,37 +2,51 @@
 const signupFormPerson = document.querySelector('#register-form-person');
 
 firebase.auth().onAuthStateChanged((user) => {
-    if (user) {
-      // User logged in already or has just logged in.
-      console.log(user.uid);
+  if (user) {
+    // User logged in already or has just logged in.
+    console.log(user.uid);
 
-      signupFormPerson.addEventListener('submit', (e) => {
-        e.preventDefault();
-    
-        //saving person info
-        const nome_completo = signupFormPerson['nome_completo'].value;
-        const cpf = signupFormPerson['cpf'].value;
-        const rg = signupFormPerson['rg'].value;
-        const orgemissor = signupFormPerson['orgemissor'].value;
-    
-    
-        db.collection('companies').doc(user.uid).collection('responsible').add({
-           nome: nome_completo,
-           cpf: cpf,
-           rg: rg,
-           orgao_emissor: orgemissor
+    signupFormPerson.addEventListener('submit', (e) => {
+      e.preventDefault();
+
+      //saving person info
+      const nome_completo = signupFormPerson['nome_completo'].value;
+      const cpf = signupFormPerson['cpf'].value;
+      const rg = signupFormPerson['rg'].value;
+      const orgemissor = signupFormPerson['orgemissor'].value;
+
+      var docData = {
+        nome: nome_completo,
+        cpf: cpf,
+        rg: rg,
+        orgao_emissor: orgemissor
+      };
+
+
+      db.collection('companies').doc(user.uid).collection('responsible').add(docData).then(function () {
+        db.collection("companies").doc(user.uid).collection('responsible').get().then(function (querySnapshot) {
+          querySnapshot.forEach(function (doc) {
+            // doc.data() is never undefined for query doc snapshots
+            console.log(doc.id, " => ", doc.data());
+
+            //add in companies data the reponsible id
+            db.collection('companies').doc(user.uid).set({
+              responsible: doc.id
+            }, { merge: true }).then(function() {
+              //after add the responsible in companies values go to painel
+              window.location.href = '/painel/index.html';
+          });
+
+          }); 
         });
-    
-        setTimeout(function() {
-            window.location.href = '/painel/index.html';
-        }, 2000);
+      });
 
     });
-    } else {
+  } else {
 
-      // User not logged in or has just logged out.
+    // User not logged in or has just logged out.
 
-    }
-  });
+  }
+});
 
 
