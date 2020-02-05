283043e343561e2784a128100c80f97c868af491
Luiz Adorno <luizadorno96@gmail.com>
2020-02-04 21:08:11 -0300

    firestore responsible auth implemented

--- painel/scripts/auth_profile.js
+++ painel/scripts/auth_profile.js
@@ -7,17 +7,58 @@ auth.onAuthStateChanged(function (user) {
 
     var docRef = db.collection("companies").doc(uid);
 
+    db.collection("companies").doc(user.uid).collection('responsible').get().then(function (querySnapshot) {
+      querySnapshot.forEach(function (doc) {
+        // doc.data() is never undefined for query doc snapshots
+        const responsible = doc.id;
+
+        db.collection("companies").doc(user.uid).collection('responsible').doc(responsible).get().then(function (doc_responsible) {
+          const nome_completo = doc_responsible.data().nome;
+          const cpf = doc_responsible.data().cpf;
+          const rg = doc_responsible.data().rg;
+          const orgao_emissor = doc_responsible.data().orgao_emissor;
+
+          //set the data from firestore
+          document.getElementById("nome_completo").innerHTML = nome_completo;
+          document.getElementById("cpf").innerHTML = cpf;
+          document.getElementById("rg").innerHTML = rg;
+          document.getElementById("orgao_emissor").innerHTML = orgao_emissor;
+        });
+
+
+
+      });
+    });
+
     docRef.get().then(function (doc) {
       if (doc.exists) {
         console.log("Document data:", doc.data());
 
-        const nome_completo;
-        const cpf;
-        const rg;
-        const orgao_emissor;
+        const nome_social = doc.data().nome_social;
+        const nome_empresa = doc.data().nome_empresa;
+        const cnpj = doc.data().cnpj;
+        const telefone = doc.data().telefone;
+        const cep = doc.data().cep;
+        const cidade = doc.data().cidade;
+        const uf = doc.data().estado;
+        const rua = doc.data().rua;
+        const bairro = doc.data().bairro;
+        const nro = doc.data().numero;
+        const complemento = doc.data().complemento;
 
 
-
+        //set the company data from firestore
+        document.getElementById("nome-empresa").innerHTML = nome_empresa;
+        document.getElementById("nome-social").innerHTML = nome_social;
+        document.getElementById("cnpj").innerHTML = cnpj;
+        document.getElementById("telefone").innerHTML = telefone;
+        document.getElementById("cep").innerHTML = cep;
+        document.getElementById("cidade").innerHTML = cidade;
+        document.getElementById("uf").innerHTML = uf;
+        document.getElementById("rua").innerHTML = rua;
+        document.getElementById("bairro").innerHTML = bairro;
+        document.getElementById("nro").innerHTML = nro;
+        document.getElementById("complemento").innerHTML = complemento;
       } else {
         // doc.data() will be undefined in this case
         console.log("No such document!");
