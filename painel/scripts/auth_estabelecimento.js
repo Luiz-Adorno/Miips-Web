5b183e914438fece7dd86aff667320f0d882034b
Luiz Adorno <luizadorno96@gmail.com>
2020-02-10 19:38:59 -0300

    add estabelecimento implemented

--- painel/scripts/auth_estabelecimento.js
+++ painel/scripts/auth_estabelecimento.js
@@ -1,5 +1,6 @@
 auth.onAuthStateChanged(function (user) {
   if (user) {
+    console.log(user.uid)
     // User is signed in.
     console.log('logged');
     if (user.photoURL != null) {
@@ -10,6 +11,26 @@ auth.onAuthStateChanged(function (user) {
     //get the value from firestore and set
     //document.getElementById("on/off").value = "Johnny Bravo";
     const string = document.getElementById("on/off").value;
+    
+
+    db.collection("companies").doc(user.uid).collection("commercialPlace").get()
+      .then(querySnapshot => {
+        querySnapshot.forEach(doc => {
+          console.log(doc.id);
+
+          db.collection("companies").doc(user.uid).collection("commercialPlace").doc(doc.id).collection("local").get().then(querySnapshot2 => {
+            querySnapshot2.forEach(doc2 => {
+              var nome_estabelecimento= doc2.data().nome_estabelecimento;
+              var estado = doc2.data().estado;
+              var cidade = doc2.data().cidade;
+              var rua = doc2.data().rua;
+              var url_photo = doc2.data().profilePhoto;
+              var status = doc2.data().status;
+              console.log(doc2.data());
+            });
+          });
+        });
+      });
 
     if (string === "Ativado") {
       document.getElementById("on/off").style.color = 'green';
